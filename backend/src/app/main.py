69be4d56815e6f31c256c1d1cd0f683fee53c681
root <root@DESKTOP-CNFB6A2.localdomain>
2020-08-22 20:04:59 -0400

    prod fix 3

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://test.localhost.com",
+    "https://mewslet.com",
+    "https://www.mewslet.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,27 @@
-function About() {
-    return <div>About</div>
+import React, { useState } from "react";
+
+const SERVER = "http://api.localhost.com";
+// const SERVER = "https://api.mewslet.com";
+
+
+console.log(`NODE_ENV: ${process.env.NODE_ENV}, SERVER: ${SERVER}`);
+
+const About = () => {
+    const [data, setData] = useState("");
+
+    fetch(`${SERVER}/`, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        }
+    }).then((response: any) => {
+        console.log(response);
+        setData(JSON.stringify(response))
+    });
+
+    return <div>Traefik Test: {data}</div>
 }
 
 export default About;
