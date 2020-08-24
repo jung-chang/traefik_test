ee5d156f662bcc44ff7217e73dba179fa4e4854a
root <root@DESKTOP-CNFB6A2.localdomain>
2020-08-23 21:52:08 -0400

    prod

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -7,34 +7,57 @@ from sqlalchemy import create_engine, Integer, Column, String
 from sqlalchemy.ext.declarative import declarative_base, declared_attr
 from sqlalchemy.orm import scoped_session, sessionmaker, Session
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    raise Exception("DATABASE_URL required.")
-print(DATABASE_URL)
-
-# SQLAlchemy engine.
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-
-# Database query builder.
-database = Database(DATABASE_URL)
-
-db_session = scoped_session(
-    sessionmaker(autocommit=False, autoflush=False, bind=engine)
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup():
-    await database.connect()
+USE_DATABASE = True
+
+if USE_DATABASE:
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    if not DATABASE_URL:
+        raise Exception("DATABASE_URL required.")
+    print(DATABASE_URL)
+
+    # SQLAlchemy engine.
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+    # Database query builder.
+    database = Database(DATABASE_URL)
+
+    db_session = scoped_session(
+        sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    )
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    @app.on_event("startup")
+    async def startup():
+        await database.connect()
 
 
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
+    @app.on_event("shutdown")
+    async def shutdown():
+        await database.disconnect()
 
+    Base = declarative_base()
+
+    class TestObject(Base):
+        __tablename__ = "testobjects"
+        id = Column(Integer, primary_key=True)
+        text = Column(String)
+
+        def __repr__(self):
+            return f"TestObject(text={self.text})"
+
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    session = SessionLocal()
+    obj = TestObject(text='testing')
+    session.add(obj)
+    session.commit()
+    session.close()
+
+    @app.get("/")
+    def root():
+        return {"Data": [str(test) for test in SessionLocal().query(TestObject).all()]}
 
 origins = [
     "http://test.localhost.com",
@@ -50,26 +73,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-Base = declarative_base()
-
-class TestObject(Base):
-    __tablename__ = "testobjects"
-    id = Column(Integer, primary_key=True)
-    text = Column(String)
-
-    def __repr__(self):
-        return f"TestObject(text={self.text})"
-
-Base.metadata.drop_all(bind=engine)
-Base.metadata.create_all(bind=engine)
-session = SessionLocal()
-obj = TestObject(text='testing')
-session.add(obj)
-session.commit()
-session.close()
-
-
 @app.get("/")
 def root():
-    return {"Data": [str(test) for test in SessionLocal().query(TestObject).all()]}
+    return {"Hello": "World"}
--- scripts/deploy.prod.sh
+++ scripts/deploy.prod.sh
@@ -5,5 +5,6 @@ set -e
 
 # xargs removes whitespace
 export HOST_IP=172.17.0.1
+export HOST_IP=$(hostname -I | xargs)
 
 docker-compose -f docker-compose.prod.yml up --build --force-recreate
